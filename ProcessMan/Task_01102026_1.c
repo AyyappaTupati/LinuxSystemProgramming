3b734c9dfff8cef6efc8c1afdbbb3ce00e262458
Ayyappa <ayyappatupati32@gmail.com>
2026-02-10 07:58:43 +0530

    new task is completed

---
+++ ProcessMan/Task_01102026_1.c
@@ -0,0 +1,64 @@
+#include"header.h"
+int delay,result,status,pid;
+int main(){
+    if(fork()==0)
+    {
+	printf("This is the child 1 process\n");
+	srand(pid = getpid());
+	delay = rand()%10+1;
+	printf("The Child 1 pid=%d and sleep=%d\n",pid,delay);
+	sleep(delay);
+       	exit(1);
+
+    }
+    else
+    {
+	result = wait(&status);
+	if(WIFEXITED(status))
+	printf("Child %d is terminated normally status=%d\n",result,WEXITSTATUS(status));
+	else if(WIFSIGNALED(status))
+	printf("Child %d is terminated of signal %d\n",result,WTERMSIG(status));
+	if(fork()==0)
+	{
+		printf("This is the child 2 process\n");
+		srand(pid = getpid());
+		delay = rand()%10+1;
+		printf("The Child 2 pid=%d and sleep=%d\n",pid,delay);
+		sleep(delay);
+		exit(1);
+	}
+	else
+	{
+		result = wait(&status);
+	   	if(WIFEXITED(status))
+		printf("Child %d is terminated normally status=%d\n",result,WEXITSTATUS(status));
+		else if(WIFSIGNALED(status))
+		printf("Child %d is terminated of signal %d\n",result,WTERMSIG(status));
+	        if(fork()==0)
+		{
+			printf("This is the child 3 process\n");
+			srand(pid = getpid());
+			delay = rand()%10+1;
+			printf("The Child 3 pid=%d and sleep=%d\n",pid,delay);
+			sleep(delay);
+			 exit(1);
+		 }
+		 else
+		 {
+			result = wait(&status);
+			if(WIFEXITED(status))
+			printf("Child %d is terminated normally status=%d\n",result,WEXITSTATUS(status));
+			else if(WIFSIGNALED(status))
+			printf("Child %d is terminated of signal %d\n",result,WTERMSIG(status));
+			sleep(delay);
+			printf("Parent is terminated\n");
+		 }
+	    }
+    }
+    return 0;
+}
+
+
+	    
+
+
